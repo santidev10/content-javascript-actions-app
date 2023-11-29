772784fc6e86482a2693b01c5a09a20f9dd933f5
santidev10 <36556977+santidev10@users.noreply.github.com>
2023-11-29 07:30:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-name: 'Hello World'
-description: 'Greet someone and record the time'
-inputs: # input parameters are stored as environment variables
-  who-to-greet:  # id of input
-    description: 'Who to greet'
-    required: true
-    default: 'World'
-outputs:
-  time: # id of output
-    description: 'The time we greeted you'
-runs:
-  using: 'node12'
-  main: 'index.js'
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
